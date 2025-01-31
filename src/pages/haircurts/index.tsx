7df57c6008027015b818b5fbae78a4bb194b0663
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-01-30 23:31:23 -0300

    edita modelos e listar

--- src/pages/haircurts/index.tsx
+++ src/pages/haircurts/index.tsx
@@ -1,13 +1,51 @@
 import { SideBar } from "@/components/SideBar";
+import { setupAPIClient } from "@/services/api";
+import { canSSRAuth } from "@/utils/canSSRAuth";
 import { Button, Flex, Heading, Stack, Switch, Text, useMediaQuery } from "@chakra-ui/react";
 import Head from "next/head";
 import Link from "next/link";
+import { ChangeEvent, useState } from "react";
 import { IoMdPricetag } from "react-icons/io";
 
+interface HaircurtsItems {
+  id: string;
+  name: string;
+  price: number | string;
+  status: boolean;
+  user_id: string;
+}
+
+interface HaircutsProps {
+  haircuts: HaircurtsItems[];
+}
 
 
-export default function Haircurts() {
+export default function Haircurts({ haircuts }: HaircutsProps) {
   const [isMobile] = useMediaQuery("(max-width: 500px)");
+  const [haircutList, setHaircutList] = useState<HaircurtsItems[]>(haircuts || []);
+  const [disableHaircut, setDisableHaircut] = useState("enabled");
+
+  async function handleDisabled(e: ChangeEvent<HTMLInputElement>) {
+    const apiClient = setupAPIClient();
+
+    if (e.target.value === "disabled") {
+      setDisableHaircut("enabled");
+      const response = await apiClient.get('/haircuts', {
+        params: {
+          status: true,
+        }
+      });
+      setHaircutList(response.data);
+    } else {
+      setDisableHaircut("disabled");
+      const response = await apiClient.get('/haircuts', {
+        params: {
+          status: false,
+        }
+      });
+      setHaircutList(response.data);
+    }
+  }
 
   return (
     <>
@@ -38,34 +76,43 @@ export default function Haircurts() {
 
             <Stack ml="auto" mb={2} align="center" direction="row">
               <Text>ATIVOS</Text>
-              <Switch colorScheme="green" size="lg" />
+              <Switch
+                colorScheme="green"
+                size="lg"
+                value={disableHaircut}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => handleDisabled(e)}
+                isChecked={disableHaircut === "disabled" ? false : true}
+              />
             </Stack>
 
           </Flex>
 
-          <Link href="/haircurts/123" style={{ width: "100%" }}>
-            <Flex
-              cursor="pointer"
-              w="100%"
-              p={4}
-              bg="barberHub.300"
-              direction={isMobile ? "column" : "row"}
-              align={isMobile ? "flex-start" : "center"}
-              rounded="4"
-              mb={2}
-              justifyContent="space-between"
-            >
-              <Flex mb={isMobile ? 2 : 0} direction="row" alignItems="center" justifyContent="center">
-                <IoMdPricetag size={28} color="#C6A05B" />
-                <Text ml={4} noOfLines={1}>
-                  Corte completo
-                </Text>
+          {haircutList.map(haircut => (
+            <Link key={haircut.id} href={`/haircurts/${haircut.id}`} style={{ width: "100%" }}>
+              <Flex
+                cursor="pointer"
+                w="100%"
+                p={4}
+                bg={disableHaircut === "enabled" ? "barberHub.300" : "transparent"}
+                border={disableHaircut === "enabled" ? "none" : "1px dashed #754D24"}
+                direction={isMobile ? "column" : "row"}
+                align={isMobile ? "flex-start" : "center"}
+                rounded="4"
+                mb={2}
+                justifyContent="space-between"
+              >
+                <Flex mb={isMobile ? 2 : 0} direction="row" alignItems="center" justifyContent="center">
+                  <IoMdPricetag size={28} color="#C6A05B" />
+                  <Text ml={4} noOfLines={1}>
+                    {haircut.name}
+                  </Text>
+                </Flex>
+
+                <Text color="#C6A05B" fontWeight="bold">R$ {haircut.price}</Text>
               </Flex>
 
-              <Text color="#C6A05B" fontWeight="bold">R$ 59.99</Text>
-            </Flex>
-
-          </Link>
+            </Link>
+          ))}
 
         </Flex>
 
@@ -75,3 +122,37 @@ export default function Haircurts() {
   )
 }
 
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+  try {
+    const apiClient = setupAPIClient(ctx);
+    const response = await apiClient.get('/haircuts', {
+      params: {
+        status: true,
+      }
+    });
+
+    if (response.data === null) {
+      return {
+        redirect: {
+          destination: '/dashboard',
+          permanent: false,
+        }
+      }
+    }
+
+    return {
+      props: {
+        haircuts: response.data,
+      }
+    }
+
+  } catch (error) {
+    console.log(error);
+    return {
+      redirect: {
+        destination: '/dashboard',
+        permanent: false,
+      }
+    }
+  }
+})
